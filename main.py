5545f67c95ab4dc501f0f51f0ba395a56e5de20b
Avi Newatia <projectavi.exe@gmail.com>
2023-10-16 12:50:47 -0400

    Bug Fixes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class LDM:
     def __init__(self, prompt: str, negative_prompt: str, seed: int, num_inference_steps: int, audio_length: float,
                  num_waveforms: int):
         # Initialise instance variables for constructor arguments
-        self.prompt = prompt # These aren't needed in this function but im keeping them as instance variables for
+        self.prompt = prompt  # These aren't needed in this function but im keeping them as instance variables for
         # possible future extension requirements
         self.negative_prompt = negative_prompt
 
@@ -19,8 +19,12 @@ class LDM:
 
         self.repository_id = "cvssp/audioldm2"
 
+        device_map = {"": 0}
+
         self.pipeline = AudioLDM2Pipeline.from_pretrained(self.repository_id,
-                                                          torch_dtype=torch.float16)  # Import BnB and quantize
+                                                          torch_dtype=torch.float16,
+                                                          device_map=device_map,
+                                                          low_cpu_mem_usage=True)  # Import BnB and quantize
         self.pipeline = self.pipeline.to("cuda")
 
         self.generator = torch.Generator("cuda").manual_seed(self.seed)
@@ -41,17 +45,17 @@ class LDM:
         self.prompt = prompt
         self.negative_prompt = negative_prompt
 
-        audio = self.pipeline(prompt, negative_prompt,
-                              num_inference_steps = self.num_inference_steps,
-                              audio_length_in_s = self.audio_length,
-                              num_waveforms_per_prompt = self.num_waveforms_on_generate,
-                              generator = self.generator,).audios
+        audio = self.pipeline(prompt, negative_prompt=negative_prompt,
+                              num_inference_steps=self.num_inference_steps,
+                              audio_length_in_s=self.audio_length,
+                              num_waveforms_per_prompt=self.num_waveforms_on_generate,
+                              generator=self.generator).audios
 
-        scipy.io.wavefile.write(filename, rate=rate, data=audio[0])
+        scipy.io.wavfile.write(filename, rate=rate, data=audio[0])
 
 
 if __name__ == "__main__":
-    prompt_inp = "The sound of a dragon breathing fire"
+    prompt_inp = "Star Wars Orchestra" #Domination issues
     negative_prompt_inp = "Low Quality"
     seed_inp = 42
 
